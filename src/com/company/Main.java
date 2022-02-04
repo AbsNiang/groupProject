52981721a2c2607ce80e518e1b405d715b11a96e
an210774 <an210774@reigate.ac.uk>
2022-02-04 12:49:07 +0000

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        frame.setSize(300, 300);
         JButton button = new JButton("Press");
         frame.getContentPane().add(button); // Adds Button to content pane of frame
         frame.setVisible(true);
---
+++ src/com/company/Utility/FileHandling.java
@@ -0,0 +1,4 @@
+package com.company.Utility;
+
+public class FileHandling {
+}
