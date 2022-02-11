d32815bb414e0785126c1223b8853310c4b7aa6d
an210774 <an210774@reigate.ac.uk>
2022-02-11 13:04:18 +0000

    Added the change of colour to the player sprite depending on the movement

--- src/com/company/GameStuff/GamePanel.java
+++ src/com/company/GameStuff/GamePanel.java
@@ -19,9 +19,9 @@ public class GamePanel extends javax.swing.JPanel implements ActionListener { //
     int cameraX;
     int offset;
 
-    public GamePanel(){
+    public GamePanel() {
 
-        player = new Player(400,300,this);
+        player = new Player(400, 300, this);
 
         reset(); //creates the level
 
@@ -29,22 +29,23 @@ public class GamePanel extends javax.swing.JPanel implements ActionListener { //
         gameTimer.schedule(new TimerTask() {
 
             @Override
-            public void run(){ //this is the main loop of the game and will run each frame
-                if(walls.get(walls.size() - 1).x < 800){ //check to see if eligible to make more walls
+            public void run() { //this is the main loop of the game and will run each frame
+                if (walls.get(walls.size() - 1).x < 800) { //check to see if eligible to make more walls
                     offset += 700; //moves the offset so the walls don't overlap
                     makeWalls(offset);
                 }
                 player.set();
-                for(Wall wall : walls) wall.set(cameraX);
+                for (Wall wall : walls) wall.set(cameraX);
                 for (int i = 0; i < walls.size(); i++) { //remove this loop to keep the prior map
-                    if(walls.get(i).x < -800) walls.remove(i); //Removes walls behind the player if it generates to many
+                    if (walls.get(i).x < -800)
+                        walls.remove(i); //Removes walls behind the player if it generates to many
                 }
                 repaint();
             }
-        }, 0,17); //delay changes when the timer starts, period is how long it waits between each tick at 17 it is set to 60fps
+        }, 0, 17); //delay changes when the timer starts, period is how long it waits between each tick at 17 it is set to 60fps
     }
 
-    public void reset(){ //this is to respawn the player when they fall or die
+    public void reset() { //this is to respawn the player when they fall or die
         player.x = 200; //this is the location where they respawn
         player.y = 150;
         cameraX = 150;
@@ -56,29 +57,31 @@ public class GamePanel extends javax.swing.JPanel implements ActionListener { //
         makeWalls(offset); //recreates the walls
     }
 
-    public void makeWalls(int offset){
+    public void makeWalls(int offset) {
         int s = 50; //wall size
         Random random = new Random();
         int index = random.nextInt(2);
 
         //add different indexes to make more of the map
-        if(index == 0){ //index determines the level which is to be played
-            for (int i = 0; i < 14; i++) walls.add(new Wall(offset + i*s, 600 ,s ,s));
-        }
-        else if(index ==1){
-            walls.add(new Wall(offset + 450,550,s,s));
-            walls.add(new Wall(offset + 350,450,s,s));
-            walls.add(new Wall(offset + 300,500,s,s));
-            walls.add(new Wall(offset + 100,550,s,s));
+        if (index == 0) { //index determines the level which is to be played
+            for (int i = 0; i < 14; i++) walls.add(new Wall(offset + i * s, 600, s, s));
+        } else if (index == 1) {
+            walls.add(new Wall(offset + 450, 550, s, s));
+            walls.add(new Wall(offset + 350, 450, s, s));
+            walls.add(new Wall(offset + 300, 500, s, s));
+            walls.add(new Wall(offset + 100, 550, s, s));
         }
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         super.paint(g); //paints over the previous frame to prevent flickering
 
         Graphics2D gtd = (Graphics2D) g; //casts graphics to 2D graphics
-
-        player.draw(gtd); //draws the player
+        if (player.isMoving()) { //changes the colour of the player, depending whether they are moving or not
+            player.drawMoving(gtd);
+        } else {
+            player.drawStill(gtd); //draws the player
+        }
 
         for (Wall wall : walls) wall.draw(gtd); //goes through and draws each wall in the walls arraylist
     }
--- src/com/company/GameStuff/Player.java
+++ src/com/company/GameStuff/Player.java
@@ -25,44 +25,47 @@ public class Player {
     boolean keyUp;
     boolean keyDown;
 
-    public Player(int x, int y, GamePanel panel){ //x & y in this line are the players starting pos
+    public Player(int x, int y, GamePanel panel) { //x & y in this line are the players starting pos
         this.panel = panel;
         this.x = x;
         this.y = y;
 
         width = 50; //width of the player
         height = 100; //height of the player
-        hitBox = new Rectangle(x,y,width,height); //sets the players hitBox to the players pos and size
+        hitBox = new Rectangle(x, y, width, height); //sets the players hitBox to the players pos and size
     }
 
-    public void set(){ //runs every frame of the game
-        if (keyLeft && keyRight || !keyLeft && !keyRight) xspeed *= 0.8; //stops the player moving if both directional keys or no keys are pressed, the deceleration speed
-        else if(keyLeft && !keyRight) xspeed --;//moves the player depending on what keys are pressed
-        else if(keyRight && !keyLeft) xspeed ++;
+    public void set() { //runs every frame of the game
+        if (keyLeft && keyRight || !keyLeft && !keyRight)
+            xspeed *= 0.8; //stops the player moving if both directional keys or no keys are pressed, the deceleration speed
+        else if (keyLeft && !keyRight) xspeed--;//moves the player depending on what keys are pressed
+        else if (keyRight && !keyLeft) xspeed++;
 
-        if(xspeed > 0 && xspeed < 0.75) xspeed = 0; //stops the player moving if the player is moving really slowly
-        if(xspeed < 0 && xspeed > -0.75) xspeed = 0;
+        if (xspeed > 0 && xspeed < 0.75) xspeed = 0; //stops the player moving if the player is moving really slowly
+        if (xspeed < 0 && xspeed > -0.75) xspeed = 0;
 
-        if(xspeed > 7) xspeed = 7; //sets a maximum speed for the player
-        if(xspeed < -7) xspeed = -7;
+        if (xspeed > 7) xspeed = 7; //sets a maximum speed for the player
+        if (xspeed < -7) xspeed = -7;
 
-        if(keyUp){
+        if (keyUp) {
             //checking when the player can jump by checking if one pixel below the player is in a wall
-            hitBox.y ++; //1 pixel below the player
-            for(Wall wall:panel.walls){ //for every wall within the walls arraylist
-                if(wall.hitBox.intersects(hitBox)) yspeed = -6; //sets if the player is touching the floor then it's allowed to jump, the speed at which the player jumps
+            hitBox.y++; //1 pixel below the player
+            for (Wall wall : panel.walls) { //for every wall within the walls arraylist
+                if (wall.hitBox.intersects(hitBox))
+                    yspeed = -6; //sets if the player is touching the floor then it's allowed to jump, the speed at which the player jumps
             }
-            hitBox.y --;//sets the hitBox back to its correct alignment
+            hitBox.y--;//sets the hitBox back to its correct alignment
         }
 
         yspeed += 0.3; //this is the speed at which gravity acts on the player
 
         //horizontal collisions
         hitBox.x += xspeed;
-        for(Wall wall:panel.walls){ //checking for all walls
-            if(hitBox.intersects(wall.hitBox)){ //if the hitBox hits a wall
+        for (Wall wall : panel.walls) { //checking for all walls
+            if (hitBox.intersects(wall.hitBox)) { //if the hitBox hits a wall
                 hitBox.x -= xspeed;
-                while(!wall.hitBox.intersects(hitBox)) hitBox.x += Math.signum(xspeed); //signum takes the value of it being + or -, allows the player to get as close to the wall without going through it
+                while (!wall.hitBox.intersects(hitBox))
+                    hitBox.x += Math.signum(xspeed); //signum takes the value of it being + or -, allows the player to get as close to the wall without going through it
                 hitBox.x -= Math.signum(xspeed);
                 panel.cameraX += x - hitBox.x; //corrects for player x value changing when hitting a wall
                 xspeed = 0;
@@ -70,12 +73,14 @@ public class Player {
             }
         }
 
+
         //vertical collisions
         hitBox.y += yspeed;
-        for(Wall wall:panel.walls){ //checking for all walls
-            if(hitBox.intersects(wall.hitBox)){ //if the hitBox hits a wall
+        for (Wall wall : panel.walls) { //checking for all walls
+            if (hitBox.intersects(wall.hitBox)) { //if the hitBox hits a wall
                 hitBox.y -= yspeed;
-                while(!wall.hitBox.intersects(hitBox)) hitBox.y += Math.signum(yspeed); //signum takes the value of it being + or -, allows the player to get as close to the wall without going through it
+                while (!wall.hitBox.intersects(hitBox))
+                    hitBox.y += Math.signum(yspeed); //signum takes the value of it being + or -, allows the player to get as close to the wall without going through it
                 hitBox.y -= Math.signum(yspeed);
                 yspeed = 0;
                 y = hitBox.y;
@@ -89,11 +94,23 @@ public class Player {
         hitBox.y = y;
 
         //death code
-        if (y> 800) panel.reset(); //if the player goes below this amount they respawn
+        if (y > 800) panel.reset(); //if the player goes below this amount they respawn
     }
 
-    public void draw(Graphics2D gtd){
-        gtd.setColor(Color.BLACK); //sets the colour of the player
-        gtd.fillRect(x,y,width,height); //fills in the player, so it is not an outline of a rectangle
+    public boolean isMoving() { //checks if the player is moving horizontally to add an animation (represented by a change in colours for now)
+        if (keyLeft && keyRight || !keyLeft && !keyRight) {
+            return false;
+        }
+        return true;
+    }
+
+    public void drawStill(Graphics2D gtd) {
+        gtd.setColor(Color.BLACK); //sets the colour of the player for when the player is still
+        gtd.fillRect(x, y, width, height); //fills in the player, so it is not an outline of a rectangle
+    }
+
+    public void drawMoving(Graphics2D gtd) {
+        gtd.setColor(Color.RED); //sets the colour of the player differently to drawStill()
+        gtd.fillRect(x, y, width, height); //fills in the player, so it is not an outline of a rectangle
     }
 }
