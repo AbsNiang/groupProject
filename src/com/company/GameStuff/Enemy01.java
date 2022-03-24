fd913c584808e27a64e400afdb32a3294bf76842
RL37 <90578623+RL37@users.noreply.github.com>
2022-03-24 21:11:26 +0000

    Update Enemy01.java

--- src/com/company/GameStuff/Enemy01.java
+++ src/com/company/GameStuff/Enemy01.java
@@ -36,38 +36,6 @@ public class Enemy01 {
         invMenu = new Rectangle(x,y,width,height);
     }
 
-    public void set(){
-        /*
-        x = startX + cameraX;
-        yspeed += 0.1; //this is the speed at which gravity acts on the player
-        xspeed += 0.3;
-        if(xspeed > 0 && xspeed < 0.75) xspeed = 0; //stops the player moving if the player is moving really slowly
-        if(xspeed < 0 && xspeed > -0.75) xspeed = 0;
-
-        if(xspeed > 3) xspeed = 3; //sets a maximum speed for the player
-        if(xspeed < -3) xspeed = -3;
-
-        //vertical collisions
-        hitBox.y += yspeed;
-        hitBox.x += xspeed;
-        for(Wall wall:panel.walls){ //checking for all walls
-            if(hitBox.intersects(wall.hitBox)){ //if the hitBox hits a wall
-                hitBox.y -= yspeed;
-                while(!wall.hitBox.intersects(hitBox)) hitBox.y += Math.signum(yspeed); //signum takes the value of it being + or -, allows the player to get as close to the wall without going through it
-                hitBox.y -= Math.signum(yspeed);
-                yspeed = 0;
-                y = hitBox.y;
-            }
-        }
-        x += xspeed;
-        y += yspeed;
-
-        hitBox.x = x; //moves the hitBox with the player
-        hitBox.y = y;
-
-         */
-    }
-
     public void draw(Graphics2D gtd){
         //gtd.setColor(Color.BLACK); //sets the colour of the player
         //gtd.fillRect(x,y,width,height); //fills in the player, so it is not an outline of a rectangle
